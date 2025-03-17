ed70b39fe5899a7eccbd0daf0fe0b25b79327dce
Harshith Mullapudi <harshithmullapudi@gmail.com>
2025-03-17 21:46:42 +0530

    Fix: google auth callback

--- apps/webapp/modules/auth/google.tsx
+++ apps/webapp/modules/auth/google.tsx
@@ -1,33 +1,48 @@
-import { Loader } from '@tegonhq/ui';
+import { Loader, useToast } from '@tegonhq/ui';
 import React from 'react';
-import { thirdPartySignInAndUp } from 'supertokens-auth-react/recipe/thirdpartyemailpassword';
+import { signInAndUp } from 'supertokens-auth-react/recipe/thirdparty';
 
 export function Google() {
+  const { toast } = useToast();
+
   async function handleGoogleCallback() {
     try {
-      const response = await thirdPartySignInAndUp();
+      const response = await signInAndUp();
 
       if (response.status === 'OK') {
         window.location.assign('/');
       } else if (response.status === 'SIGN_IN_UP_NOT_ALLOWED') {
-        console.error(response.reason);
+        // the reason string is a user friendly message
+        // about what went wrong. It can also contain a support code which users
+        // can tell you so you know why their sign in / up was not allowed.
+        toast({ title: 'Error', description: response.reason });
       } else {
         // SuperTokens requires that the third party provider
         // gives an email for the user. If that's not the case, sign up / in
         // will fail.
 
-        console.error(
-          'No email provided by social login. Please use another form of login',
-        );
+        // As a hack to solve this, you can override the backend functions to create a fake email for the user.
+
+        toast({
+          title: 'Error',
+          description:
+            'No email provided by social login. Please use another form of login',
+        });
+
         window.location.assign('/auth'); // redirect back to login page
       }
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (err: any) {
       if (err.isSuperTokensGeneralError === true) {
         // this may be a custom error message sent from the API by you.
-        console.error(err.message);
+        toast({
+          title: 'Error',
+          description: err.message,
+        });
       } else {
-        console.error('Oops! Something went wrong.');
+        toast({
+          title: 'Error',
+          description: 'Oops! Something went wrong.',
+        });
       }
     }
   }
@@ -36,5 +51,5 @@ export function Google() {
     handleGoogleCallback();
   }, []);
 
-  return <Loader text="Signing up..." />;
+  return <Loader text="Signing in..." />;
 }
