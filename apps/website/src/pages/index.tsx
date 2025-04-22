9445cb8637822e0ad0e82cd638dbda498f9b3486
Harshith Mullapudi <harshithmullapudi@gmail.com>
2025-04-22 12:44:06 +0530

    fix: build error in website

--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { RiDiscordFill, RiGithubFill } from '@remixicon/react';
 import {
   AI,
-  Button,
   buttonVariants,
   CalendarLine,
   cn,
@@ -9,6 +8,7 @@ import {
   Project,
 } from '@tegonhq/ui';
 import Image from 'next/image';
+
 import { Feature } from 'src/components';
 
 export default function Index() {
