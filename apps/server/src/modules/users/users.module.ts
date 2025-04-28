b009f97f9e79a3dca63de314062f48ed2af9f62e
Manik <manik01july@gmail.com>
2025-04-28 12:07:25 +0530

    Feat:add getting started list

--- apps/server/src/modules/users/users.module.ts
+++ apps/server/src/modules/users/users.module.ts
@@ -3,13 +3,15 @@ import { PrismaModule, PrismaService } from 'nestjs-prisma';
 
 import AIRequestsService from 'modules/ai-requests/ai-requests.services';
 import { SupertokensService } from 'modules/auth/supertokens/supertokens.service';
+import { ListsModule } from 'modules/lists/lists.module';
+import { TasksModule } from 'modules/tasks/tasks.module';
 import WorkspacesService from 'modules/workspaces/workspaces.service';
 
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ListsModule, TasksModule],
   controllers: [UsersController],
   providers: [
     PrismaService,
--- apps/server/src/modules/workspaces/workspaces.module.ts
+++ apps/server/src/modules/workspaces/workspaces.module.ts
@@ -3,13 +3,15 @@ import { Module } from '@nestjs/common';
 import { PrismaModule, PrismaService } from 'nestjs-prisma';
 
 import AIRequestsService from 'modules/ai-requests/ai-requests.services';
+import { ContentModule } from 'modules/content/content.module';
+import { ListsModule } from 'modules/lists/lists.module';
 import { UsersService } from 'modules/users/users.service';
 
 import { WorkspacesController } from './workspaces.controller';
 import WorkspacesService from './workspaces.service';
 
 @Module({
-  imports: [PrismaModule, HttpModule],
+  imports: [PrismaModule, HttpModule, ListsModule, ContentModule],
   controllers: [WorkspacesController],
   providers: [
     WorkspacesService,
