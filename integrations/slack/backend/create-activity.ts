7a3665a15fd42f3ea56852ef257149fae433b5d2
Manoj K <saimanoj58@gmail.com>
2025-05-09 21:36:22 +0530

    Fix: slack webhook activity

--- integrations/slack/backend/create-activity.ts
+++ integrations/slack/backend/create-activity.ts
@@ -1,6 +1,8 @@
 import { IntegrationAccount } from '@tegonhq/sigma-sdk';
 import axios from 'axios';
 
+import { getChannelDetails, getUserDetails } from './utils';
+
 export const createActivityEvent = async (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   eventBody: any,
@@ -9,12 +11,38 @@ export const createActivityEvent = async (
   const { eventData } = eventBody;
 
   if (eventData.event.type === 'star_added') {
-    const message = eventData.event.item.message;
+    const event = eventData.event;
+
+    const integrationConfiguration = integrationAccount.integrationConfiguration as Record<
+      string,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      any
+    >;
+    if (!integrationConfiguration) {
+      throw new Error('Integration configuration not found');
+    }
+    const eventMessage = event.item.message;
+
+    const mentionedUsers = getMentionUsers(event.item.message.text);
+    const [userDetails, channelDetails] = await Promise.all([
+      getUserDetails([eventMessage.user, ...mentionedUsers], integrationConfiguration.access_token),
+      getChannelDetails(event.item.channel, integrationConfiguration.access_token),
+    ]);
+
+    const userIdMap = new Map(userDetails.map((user) => [user.id, user]));
+
+    const eventMessageText = eventMessage.text.replace(/<@U\w+>/g, (match: string) => {
+      const userId = match.replace(/<@|>/g, '');
+      const user = userIdMap.get(userId);
+      return user ? `@${user.real_name}|${userId}` : match;
+    });
+
+    const text = `Message from user ${userIdMap.get(eventMessage.user)?.real_name}(${eventMessage.user}) in channel ${channelDetails.name}(${channelDetails.id}) at ${eventMessage.ts}. Content: '${eventMessageText}'`;
 
     const activity = {
-      sourceURL: message.permalink,
-      sourceId: message.ts,
-      text: message.text,
+      sourceURL: eventMessage.permalink,
+      sourceId: eventMessage.ts,
+      text,
       integrationAccountId: integrationAccount.id,
       taskId: null,
     };
@@ -23,3 +51,8 @@ export const createActivityEvent = async (
   }
   return { message: `Processed activity from slack` };
 };
+
+function getMentionUsers(message: string): string[] {
+  const mentionUsers = message.matchAll(/<@U\w+>/g);
+  return Array.from(mentionUsers).map((match) => match[0].replace(/<@|>/g, ''));
+}
