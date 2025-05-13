afd32927357ca951370219e32137908cc90fd103
Harshith Mullapudi <harshithmullapudi@gmail.com>
2025-05-13 10:56:46 +0530

    Fix: remove relevant context from server

--- apps/server/src/modules/activity/activity.module.ts
+++ apps/server/src/modules/activity/activity.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { PrismaModule, PrismaService } from 'nestjs-prisma';
 
 import { UsersService } from 'modules/users/users.service';
-import { WorkspacesModule } from 'modules/workspaces/workspaces.module';
 
 import { ActivityController } from './activity.controller';
 import ActivityService from './activity.service';
 
 @Module({
-  imports: [PrismaModule, WorkspacesModule],
+  imports: [PrismaModule],
   controllers: [ActivityController],
   providers: [PrismaService, ActivityService, UsersService],
   exports: [ActivityService],
